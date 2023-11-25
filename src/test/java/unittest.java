3655ad8592ebee2598a959471cd6c931ed830151
Ali Khan <nangyaly0@gmail.com>
2023-11-25 09:05:26 -0500

    Update unittest.java

--- src/test/java/unittest.java
+++ src/test/java/unittest.java
@@ -14,19 +14,18 @@ public class unittest {
         // Set the path to your ChromeDriver executable
         System.setProperty("webdriver.chrome.driver", "/home/runner/bin/chromedriver");
 
+        // Create a new instance of the Chrome driver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        //options.AddArgument("no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+
         // Print ChromeDriver version
         System.out.println("ChromeDriver version: " + System.getProperty("webdriver.chrome.driver.version"));
 
         // Print Chrome browser version
         Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
         System.out.println("Chrome browser version: " + caps.getCapability("browserVersion"));
-
-
-        // Create a new instance of the Chrome driver
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        //options.AddArgument("no-sandbox");
-        WebDriver driver = new ChromeDriver(options);
 
         // Navigate to the login page
         driver.get("/home/runner/work/Github-Action-Project/Github-Action-Project/WebContent/login.html");
