abb49edc8f7eeaecca4f3db56d5d4a52eb42e4c9
Ali Khan <alikhan@Alis-MacBook-Air.local>
2023-11-24 21:03:20 -0500

    push

--- src/test/java/unittest.java
+++ src/test/java/unittest.java
@@ -6,15 +6,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class unittest {
     @Test
-    public static void testLoginPage() {
+    public void testLoginPage() {
         // Set the path to your ChromeDriver executable
-        System.setProperty("webdriver.chrome.driver", "/home/runner/bin/chromedriver  ");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
         // Create a new instance of the Chrome driver
         WebDriver driver = new ChromeDriver();
 
         // Navigate to the login page
-        driver.get("/home/runner/work/Github-Action-Project/Github-Action-Project/WebContent/login.html");
+        driver.get("/Users/alikhan/Documents/Lambton_College/3rd_semester/CI-CD/Github-Action-Project/WebContent/login.html");
 
         // Find the element by its ID
         WebElement welcomeDiv = driver.findElement(By.id("topmid"));
